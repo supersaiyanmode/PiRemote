efa00571b7a6b31c6ea12007d58a378f2cc8fd4f
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-30 23:25:01 -0700

    Add result type to Operations.

--- app/src/main/java/com/srivatsaniyer/piremote/messaging/Operation.java
+++ app/src/main/java/com/srivatsaniyer/piremote/messaging/Operation.java
@@ -1,27 +1,32 @@
 package com.srivatsaniyer.piremote.messaging;
 
+import com.srivatsaniyer.piremote.messaging.exceptions.BadOperation;
+
 /**
  * Created by sriiyer on 9/23/17.
  */
 
-enum Operation {
+public enum Operation {
     ENQUEUE("enqueue"),
     DEQUEUE("dequeue"),
-    INFORM("inform");
+    INFORM("inform"),
+    RESULT("result");
 
     public String serialize() {
         return this.tag;
     }
 
-    public static Operation parse(String op) {
+    public static Operation parse(String op) throws BadOperation {
         if ("enqueue".equalsIgnoreCase(op)) {
             return Operation.ENQUEUE;
         } else if ("dequeue".equalsIgnoreCase(op)) {
             return Operation.DEQUEUE;
         } else if ("inform".equalsIgnoreCase(op)) {
             return Operation.INFORM;
+        } else if ("result".equalsIgnoreCase(op)) {
+            return Operation.RESULT;
         }
-        return null;
+        throw new BadOperation(op);
     }
 
     Operation(String tag) {
