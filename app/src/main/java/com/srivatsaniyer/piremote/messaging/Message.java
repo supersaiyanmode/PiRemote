8589134096eb1ac55baf6bca68031ef3568a5e32
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-30 23:23:18 -0700

    Bug fixes and code cleanup. Update test cases.

--- app/src/main/java/com/srivatsaniyer/piremote/messaging/Message.java
+++ app/src/main/java/com/srivatsaniyer/piremote/messaging/Message.java
@@ -1,11 +1,14 @@
 package com.srivatsaniyer.piremote.messaging;
 
 import com.google.gson.Gson;
-import com.srivatsaniyer.piremote.messaging.exceptions.MessageParseException;
+import com.srivatsaniyer.piremote.messaging.exceptions.InvalidMessageStructure;
+import com.srivatsaniyer.piremote.messaging.exceptions.MessagingException;
+import com.srivatsaniyer.piremote.messaging.exceptions.RequiredFieldsMissing;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,30 +20,42 @@ import java.util.Set;
  * Created by sriiyer on 9/23/17.
  */
 
-class Message<T> {
-    public Message(String queue, Operation op) {
-        this(queue, op, null);
+public class Message<T> {
+
+    public Message(Operation op) {
+        this(op, null);
     }
 
-    public Message(String queue, Operation op, T data) {
-        this.queue = queue;
+    public Message(Operation op, T data) {
         this.operation = op;
         this.data = data;
+        this.headers = new HashMap<String, String>();
     }
 
     public void write(PrintWriter writer) {
         writer.println("OP " + this.operation.serialize());
-        writer.println("Q " + this.queue);
-        writer.println("MSG " + new Gson().toJson(data));
+        if (data != null) {
+            writer.println("MSG " + new Gson().toJson(data));
+        }
+        for (Map.Entry<String, String> entry: headers.entrySet()) {
+            writer.println(entry.getKey() + " " + entry.getValue());
+        }
         writer.println("");
     }
 
-    public static <X> Message<X> read(BufferedReader reader, Class<X> clazz) throws IOException, MessageParseException {
+    public static <X> Message<X> read(BufferedReader reader, Type type)
+            throws MessagingException {
         List<String> lines = new ArrayList<String>();
         while (true) {
-            String line = reader.readLine();
+            final String line;
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                throw new InvalidMessageStructure("IOException occured.");
+            }
+
             if (line == null) {
-                throw new MessageParseException("Blank line expected.");
+                throw new InvalidMessageStructure("Blank line expected.");
             }
             if (line.isEmpty()) {
                 // We are done reading the message. break out of loop.
@@ -51,14 +66,13 @@ class Message<T> {
 
         Set<String> required_fields = new HashSet<String>() {{
             add("OP");
-            add("Q");
         }};
         Map<String, String> fields = new HashMap<>();
 
         for (String line: lines) {
             String[] parts = line.split(" ", 2);
             if (parts.length != 2) {
-                throw new MessageParseException("Bad headers.");
+                throw new InvalidMessageStructure("Bad headers.");
             }
             fields.put(parts[0].trim(), parts[1].trim());
         }
@@ -66,24 +80,24 @@ class Message<T> {
         required_fields.removeAll(fields.keySet());
         if (!required_fields.isEmpty()) {
             System.out.println(required_fields);
-            throw new MessageParseException("Required fields missing.");
+            throw new RequiredFieldsMissing("Required fields missing.");
         }
 
         String data = fields.get("MSG");
         Operation op = Operation.parse(fields.get("OP"));
-        if (op == null) {
-            throw new MessageParseException("Bad Operation");
-        }
-        if (data != null) {
-            X obj = new Gson().fromJson(data, clazz);
-            return new Message<X>(fields.get("Q"), op, obj);
-        } else {
-            return new Message<X>(fields.get("Q"), op);
-        }
-    }
+        fields.remove("OP");
 
-    public String getQueue() {
-        return queue;
+        if (data != null) {
+            fields.remove("MSG");
+            X obj = new Gson().fromJson(data, type);
+            Message<X> msg = new Message<X>(op, obj);
+            msg.getHeaders().putAll(fields);
+            return msg;
+        } else {
+            Message<X> msg = new Message<X>(op);
+            msg.getHeaders().putAll(fields);
+            return msg;
+        }
     }
 
     public Operation getOperation() {
@@ -94,8 +108,14 @@ class Message<T> {
         return data;
     }
 
-    private String queue;
-    private Operation operation;
-    private T data;
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
 
+
+    private final Operation operation;
+    private final T data;
+
+
+    private final HashMap<String, String> headers;
 }
