06328d49092ef7dffd46df31ca6b141814fd5c44
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-08 13:51:37 -0700

    Code clean up.

--- app/src/main/java/com/srivatsaniyer/piremote/messaging/MessagingClient.java
+++ app/src/main/java/com/srivatsaniyer/piremote/messaging/MessagingClient.java
@@ -1,26 +1,13 @@
 package com.srivatsaniyer.piremote.messaging;
 
-import android.util.Log;
-
-import com.google.gson.Gson;
-import com.srivatsaniyer.piremote.messaging.exceptions.MessageParseException;
-import com.srivatsaniyer.piremote.messaging.exceptions.InvalidMessageStructure;
 import com.srivatsaniyer.piremote.messaging.exceptions.MessagingException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.lang.reflect.Type;
 import java.net.Socket;
-import java.nio.charset.Charset;
-
-/**
- * Created by sriiyer on 9/23/17.
- */
-
 
 public class MessagingClient {
     public MessagingClient(ServerSpecification spec) throws IOException{
