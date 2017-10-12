58b58c10a26c7332ecb04addc646cf2126605d96
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-11 20:39:48 -0700

    Message received needs additional Generic type parameters to pass to Gson decoder.

--- app/src/main/java/com/srivatsaniyer/piremote/messaging/MessageReceiver.java
+++ app/src/main/java/com/srivatsaniyer/piremote/messaging/MessageReceiver.java
@@ -6,18 +6,19 @@ import com.google.gson.reflect.TypeToken;
 import com.srivatsaniyer.piremote.messaging.exceptions.MessagingException;
 
 import java.lang.reflect.Type;
-import java.util.Arrays;
 
 /**
  * Created by thrustmaster on 9/30/17.
  */
 
 public abstract class MessageReceiver<T> {
-    public MessageReceiver(String queueName, MessagingClient client, Type... paramTypes) {
-        this.paramTypes = paramTypes;
+    public MessageReceiver(String queueName, MessagingClient client, Type mainType, Type... types) {
         this.client = client;
         this.queueName = queueName;
         this.active = true;
+
+        this.mainType = mainType;
+        this.typeArgs = types;
 
         final MessageReceiver receiver = this;
         this.receiverThread = new Thread(new Runnable() {
@@ -33,15 +34,13 @@ public abstract class MessageReceiver<T> {
     }
 
     public void run() {
-        Type mainType = this.paramTypes[0];
-        Type[] params = Arrays.copyOfRange(this.paramTypes, 1, this.paramTypes.length);
-        Type type = TypeToken.getParameterized(mainType, params).getType();
         while (this.active) {
             Message<Void> dequeueMsg = new Message<Void>(Operation.DEQUEUE);
             dequeueMsg.getHeaders().put("Q", this.queueName);
             client.writeMessage(dequeueMsg);
             try {
-                this.onMessage(client.<T>readMessage(type));
+                Type messageType = TypeToken.getParameterized(mainType, typeArgs).getType();
+                this.onMessage(client.<T>readMessage(messageType));
             } catch (MessagingException e) {
                 Log.w("MessageReceiver", "Got an exception.", e);
             }
@@ -60,5 +59,6 @@ public abstract class MessageReceiver<T> {
     private final String queueName;
     private volatile boolean active;
     private final Thread receiverThread;
-    private final Type[] paramTypes;
+    private final Type mainType;
+    private final Type[] typeArgs;
 }
